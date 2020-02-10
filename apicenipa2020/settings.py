1b2ecbcbebba8777e4cfd39ead04775f94493fa1
guedesemerson <guedes.emerson@hotmail.com>
2020-02-09 22:31:48 -0200

    Configuring the app

--- apicenipa2020/settings.py
+++ apicenipa2020/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1l)flai2fi(c(6+b^!c-t29cho-)xq%__!d=+aw5c&h0pptdij'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['apicenipa2020.herokuapp.com','127.0.0.1']
 
